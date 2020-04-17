29c75e6515b2c39a4e2451452bb77a4302eb39a0
irmiller <63781021+irmiller@users.noreply.github.com>
2020-04-17 14:58:48 +0200

    Update bypass.js

--- src/bypass.js
+++ src/bypass.js
@@ -1,5 +1,6 @@
 function bypass(req, res, buffer) {
-  res.setHeader('x-proxy-bypass', 1)
+  res.setHeader('x-original-size', req.params.originSize)
+  res.setHeader('x-bytes-saved', 0)
   res.setHeader('content-length', buffer.length)
   res.status(200)
   res.write(buffer)
